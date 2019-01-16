4face440b85d56573a9dfe482729093e29878e44
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-01-16 20:35:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,13 +17,19 @@ class App < Sinatra::Base
   # Code your final two routes here:
   get "/goodbye/:name" do
     @user_name = params[:name]
+    #params is a hash, anything with a : is a key
     "Goodbye, #{@user_name}."
   end
 
   get "/multiply/:num1/:num2" do
+<<<<<<< HEAD
     200
     @product = params[:num1].to_i * params[:num2].to_i
     @product.to_s
 
+=======
+    @product = params[:num1].to_i * params[:num2].to_i
+    @product.to_s
+>>>>>>> 0bfb8c511ec4cbb39638fad0d5701c2c7cba28d2
   end
 end
